e7e3497fb50ea771d876eb7bced8ce1c1b5c34cd
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-11-26 20:27:46 -0300

    resolvidos exercicios 1 e 2

---
+++ exercises/bloco_4/dia_2/exercise2.js
@@ -0,0 +1,11 @@
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0
+// 2 - Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+for (let index = 0; index < numbers.length; index += 1){
+  total += numbers[index];
+
+}
+
+console.log(total);
