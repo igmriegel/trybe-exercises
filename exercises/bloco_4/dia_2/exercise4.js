41fd6fc944beb080cfd633f96172f61bf9876fca
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-11-27 13:14:44 -0300

    resolvido exercicio 4.

---
+++ exercises/bloco_4/dia_2/exercise4.js
@@ -0,0 +1,19 @@
+// 4 - Com o mesmo código do exercício anterior, caso valor final seja maior que 20, 
+// imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0
+let average = 0
+
+for (let index = 0; index < numbers.length; index += 1){
+  total += numbers[index];
+
+}
+
+average = total / numbers.length
+
+if (average > 20){
+  console.log('valor maior que 20')
+} else {
+  console.log('valor menor ou igual a 20')
+}
