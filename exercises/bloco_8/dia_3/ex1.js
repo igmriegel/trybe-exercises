f24ae2d037619c89136752f9f903fc34735ec575
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-01-20 14:17:18 -0300

    Adicionados arquivos de exercicio

---
+++ exercises/bloco_8/dia_3/ex1.js
@@ -0,0 +1,18 @@
+// Todos os exercícios devem ser realizados utilizando reduce , e se necessário outra HOF , a informação será citada no enunciado.
+// Dada uma matriz de matrizes, transforme em uma única matriz.
+
+
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
