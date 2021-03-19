b92b1c74688004d5db78f2ed89fd26b54be72da4
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-03-19 05:08:13 -0300

    Concluidos exercicios de fixaçao do conteudo de RTL.

---
+++ exercises/bloco_15/dia_1/testes-react/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+import ValidEmail from './ValidEmail'
+
+import './App.css';
+import logo from './logo.svg'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+        </label>
+        <input
+          id="id-email"
+          value={email}
+          type="email"
+          onChange={(e) => this.changeEmail(e.target.value)}
+        />
+        <input
+          id="btn-enviar"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={() => this.changeSaveEmail(email)}
+        />
+        <input id="btn-id" type="button" value="Voltar" />
+        <ValidEmail email={saveEmail}/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
