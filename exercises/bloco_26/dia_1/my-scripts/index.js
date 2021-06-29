6ddd0f4fa344d1aad06f636bda122d43a5faf11b
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-29 10:32:05 -0300

    Correçoes do dia um e montagem de arquivo index

--- exercises/bloco_26/dia_1/my-scripts/index.js
+++ exercises/bloco_26/dia_1/my-scripts/index.js
@@ -1,32 +1,17 @@
 const readline = require('readline-sync');
 
-// Criamos uma lista dos scripts disponíveis
-// Utilizamos objetos com `name` e `script` para facilitar a criação da lista que será exibida
 const scripts = [
   { name: 'Calcular IMC', script: './imc.js' },
   { name: 'Calcular velocidade média', script: './velocidade.js' },
   { name: 'Jogo de adivinhação', script: './sorteio.js' },
 ];
 
-// Iteramos sobre os scripts para criar a lista numerada
-let mensagem = scripts
-  .map((script, index) => `${index + 1} - ${script.name}`);
- 
-// Adicionamos uma linha a mais no começo da mensagem
-mensagem.unshift('Escolha um número para executar o script correspondente');
+const message = scripts.map((item, index) => `${index + 1} - ${ item.name }`).join('\n')
 
-// Juntamos todos os elementos em uma string, separando-os por uma quebra de linha
-mensagem = mensagem.join('\n');
+const selectedScript = scripts[
+  readline.questionInt(
+    `Qual script você deseja executar? \n${ message }
+    Resposta => `) - 1
+].script;
 
-const scriptNumber = readline.questionInt(mensagem) - 1;
-
-const script = scripts[scriptNumber];
-
-if (!script) return console.log('Número inválido. Saindo');
-
-// Chamamos o script selecionado
-// Note que, no dia a dia, é mais comum utilizarmos outras formas de executar arquivos externos
-// No entanto, para fins didáticos, o `require` nos atende por enquanto.
-require(script.script);
-
-// Implementação conforme o gabarito do exercicio
+require(selectedScript);
