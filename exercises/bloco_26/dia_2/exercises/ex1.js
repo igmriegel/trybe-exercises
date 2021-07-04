772565a5e2f0e7d0d75dd1b330b119a5fc73e9cd
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-07-04 01:16:31 -0300

    ex 01

---
+++ exercises/bloco_26/dia_2/exercises/ex1.js
@@ -0,0 +1,19 @@
+const calcular = async (a, b, c) => {
+  return new Promise((res, rej) => {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      rej('Informe apenas números');
+    }
+
+    const result = ( (a + b) * c );
+
+    if (result < 50) {
+      rej('Valor muito baixo');
+    }
+
+    res(result);
+  });
+}
+
+(async function () {
+  console.log(await calcular(1, 2, 3))
+})()
