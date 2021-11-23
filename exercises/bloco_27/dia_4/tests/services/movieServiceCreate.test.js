48a8b7de8889af8404ead3e5cc3b6042f0565b21
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-11-23 15:31:24 -0300

    re-start do bloco 29

---
+++ exercises/bloco_27/dia_4/tests/services/movieServiceCreate.test.js
@@ -0,0 +1,7 @@
+describe("test ok", () => {
+  it('test me', () => {});
+  it('test me one', () => {});
+  it('test me two', () => {});
+  it('test me three', () => {});
+  it('test me four', () => {});
+});
