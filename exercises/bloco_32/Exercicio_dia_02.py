5bf8303977410882895229a26d03f098986ac278
Igor Marcos Riegel <igor.riegel@gmail.com>
2022-01-25 16:46:35 -0300

    Adicionados arquivos para desenvolvimento dos exercicios

--- exercises/bloco_32/Exercicio_dia_02.py
+++ exercises/bloco_32/Exercicio_dia_02.py
@@ -47,6 +47,7 @@
 # um número de letras do nome daquele pokemon igual ao número de erros.
 # Exemplo: o pokemon sorteado pelo programa é butterfree , a pessoa usuária
 # chuta charizard , o programa deve exibir b . Em seguida, a pessoa chuta
-# blastoise , o programa deve exibir bu e assim por diante até a pessoa acertar.
-# Você pode utilizar o método choice do modulo random para sortear aleatoriamente
-# um pokemon. Ex: random.choice(pokemon) -> Saída: {"name": "Pikachu", }
+# blastoise , o programa deve exibir bu e assim por diante até a pessoa acertar
+# Você pode utilizar o método choice do modulo random para sortear
+# aleatoriamente um pokemon.
+# Ex: random.choice(pokemon) -> Saída: {"name": "Pikachu", }
